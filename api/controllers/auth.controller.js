6d3bf77203449e65f5d522a3d861a6588d005bcf
Jeet <jeetmaheshwari1919@gmail.com>
2024-06-25 15:58:55 +0530

    add a middleware and a function to handle error

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,29 +1,31 @@
-import User from '../models/user.model.js'
+import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
+import { errorHandler } from '../utils/error.js';
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
 
     if (!username || !email || !password || username === '' || email === '' || password === '') {
-        return res.status(400).json({
-            message: 'All fields are required'
-        })
+        return next(errorHandler(400, 'All fields are required'));
     }
-
-    const hashedPassword = bcryptjs.hashSync(password, 10);
-
-    const newUser = new User({
-        username,
-        email,
-        password:hashedPassword
-    });
 
     try {
-        await newUser.save();
-        return res.status(200).json('Signup Sucessful');
-    } catch (error) {
-        res.status(500).json({
-            message: error.message
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return next(errorHandler(409, 'User already exists'));
+        }
+
+        const hashedPassword = await bcryptjs.hash(password, 10);
+
+        const newUser = new User({
+            username,
+            email,
+            password: hashedPassword
         });
+
+        await newUser.save();
+        return res.status(201).json('Signup successful');
+    } catch (error) {
+        next(error);
     }
-}
+};
--- api/index.js
+++ api/index.js
@@ -21,6 +21,16 @@ app.use(express.json());
 app.use('/api/user', userRouters);
 app.use('/api/auth', authRoutes);
 
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal server message';
+    res.status(statusCode).json({ 
+        sucess: false,
+        statusCode,
+        message 
+    })
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
