1fa274976c104cb40c3e4ce5319b4eee0aade41f
Jeet <jeetmaheshwari1919@gmail.com>
2024-06-30 16:55:34 +0530

    add admin fucntionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -53,7 +53,7 @@ export const signin = async (req, res, next) => {
         const { password: pass, ...rest } = validUser._doc;
 
         const token = jwt.sign(
-            { id: validUser._id },
+            { id: validUser._id, isAdmin: validUser.isAdmin },
             process.env.JWT_SECRET,
             { expiresIn: '5h' }
         );
@@ -70,7 +70,7 @@ export const google = async (req, res, next) => {
         const user = await User.findOne({ email });
         if (user) {
             const token = jwt.sign(
-                { id: user._id },
+                { id: user._id, isAdmin: user.isAdmin },
                 process.env.JWT_SECRET,
                 { expiresIn: '5h' }
             );
@@ -88,7 +88,7 @@ export const google = async (req, res, next) => {
             await newUser.save();
 
             const token = jwt.sign(
-                { id: newUser._id },
+                { id: newUser._id, isAdmin: newUser.isAdmin },
                 process.env.JWT_SECRET,
                 { expiresIn: '5h' }
             );
