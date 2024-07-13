044413a09f2b6581fef1965c50452de9d34e4703
Jeet <jeetmaheshwari1919@gmail.com>
2024-07-13 17:38:56 +0530

    Complete about and project pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,29 @@
 
 function About() {
   return (
-    <div>about</div>
+    <div className="flex flex-col items-center justify-center min-h-[80vh]">
+      <div className="max-w-2xl mx-auto p-3 text-center">
+        <div>
+          <h1 className="text-3xl font-bold my-7">About Jeet&apos;s Blog</h1>
+          <div className="text-md text-gray-500 flex flex-col gap-6">
+
+            <p>
+              Welcome to Jeet&apos;s Blog! This blog was created by Jeet Maheshwari as a personal project to share my thoughts and ideas with the world. I am a passionate developer who loves to write about technology, coding and everything in between.
+            </p>
+
+            <p>
+              Jeet&apos;s Blog is ablog that I created to share my thought and ideas with the world.I am a student and I love to write about my experiences and things I have learned.
+              I hope you enjoy reading my blog.
+            </p>
+
+            <p>
+              We encourage you to leave comments on our posts and engage with other readers. You can like other people&apos;s comments and reply to them as well. We believe that a community of learners can help each other grow and improve.
+            </p>
+
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,7 +1,12 @@
+import CallToAction from '../components/CallToAction'
 
 function Projects() {
   return (
-    <div>Projects</div>
+    <div className='min-h-[80vh] max-w-2xl mx-auto flex flex-col justify-center items-center gap-5'>
+      <h1 className='text-3xl font-bold'>Projects</h1>
+      <p className='text-md text-gray-500'>Build fun and engaging projects while learning HTML, CSS, and JavaScript!</p>
+      <CallToAction />
+    </div>
   )
 }
 
