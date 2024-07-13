82bba31f33400d1a23a6780a0d31374909341d80
Jeet <jeetmaheshwari1919@gmail.com>
2024-07-13 17:57:26 +0530

    add pre installation

--- api/index.js
+++ api/index.js
@@ -3,9 +3,10 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouters from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
-import postRoutes from './routes/post.route.js'; 
+import postRoutes from './routes/post.route.js';
 import commentRoutes from './routes/comment.route.js';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 
 dotenv.config();
 
@@ -17,6 +18,8 @@ mongoose.connect(process.env.MONGO_URI)
         console.error("Error connecting to MongoDB:", error);
     });
 
+const __dirname = path.resolve();
+
 const app = express();
 
 app.use(express.json());
@@ -24,16 +27,22 @@ app.use(cookieParser());
 
 app.use('/api/user', userRouters);
 app.use('/api/auth', authRoutes);
-app.use('/api/post',postRoutes);
-app.use('/api/comment',commentRoutes);
+app.use('/api/post', postRoutes);
+app.use('/api/comment', commentRoutes);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
     const message = err.message || 'Internal server message';
-    res.status(statusCode).json({ 
+    res.status(statusCode).json({
         sucess: false,
         statusCode,
-        message 
+        message
     })
 });
 
