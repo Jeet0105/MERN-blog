9d8e79374df69ca2e6be0da566b83caffa420741
Jeet <jeetmaheshwari1919@gmail.com>
2024-07-13 18:21:56 +0530

    MongoDb Connection

--- api/index.js
+++ api/index.js
@@ -12,7 +12,7 @@ dotenv.config();
 
 mongoose.connect(process.env.MONGO_URI)
     .then(() => {
-        console.log("Connected to MongoDB");
+        console.log("Connected to MongoDB Atlas");
     })
     .catch((error) => {
         console.error("Error connecting to MongoDB:", error);
